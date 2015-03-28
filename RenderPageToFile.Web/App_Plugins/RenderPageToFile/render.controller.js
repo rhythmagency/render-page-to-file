f93c3ef93ee4ef1fc96dd3a8c55c9719e85251de
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-03-27 21:14:20 -0700

    Fixes, refactoring, improved error handling.

--- RenderPageToFile.Web/App_Plugins/RenderPageToFile/render.controller.js
+++ RenderPageToFile.Web/App_Plugins/RenderPageToFile/render.controller.js
@@ -2,35 +2,53 @@ angular.module("umbraco").controller("RenderPageToFile",
     function ($rootScope, $scope, $http, dialogService, editorState) {
 
         // Variables.
+        var defaultError = "An error occurred.";
+        var url = "/umbraco/backoffice/RenderPageToFile/Render/RenderPage";
+
+        // Initialize status variables.
         $scope.inProgress = false;
         $scope.wasError = false;
         $scope.wasSuccess = false;
+        $scope.errorMessage = null;
 
-        // Stores the current page as HTML.
+        // Stores the current page as a file.
         $scope.store = function () {
 
             // Variables.
             var pageId = editorState.current.id;
             var path = $scope.model.config.path;
-            var url = "/umbraco/backoffice/RenderPageToFile/Render/RenderPage";
+            var data = {
+                PageId: pageId,
+                FilePath: path
+            };
 
-            // Attempt to store to file.
+            // Reset status variables.
             $scope.wasError = false;
             $scope.wasSuccess = false;
             $scope.inProgress = true;
-            $http.post(url, { pageId: pageId, path: path }).success(function (data) {
-                $scope.inProgress = false;
-                $scope.wasSuccess = data.Success;
-                $scope.wasError = !data.Success;
-                if ($scope.wasError) {
-                    $scope.errorMessage = data.FailureReason;
-                }
-            }).error(function () {
-                $scope.inProgress = false;
-                $scope.wasError = true;
-                $scope.wasSuccess = false;
-            });
+            $scope.errorMessage = null;
+
+            // Attempt to store to file.
+            $http.post(url, data).success(handleSuccess).error(handleError);
 
         };
+
+        // Handles a successful request.
+        function handleSuccess(data) {
+            $scope.inProgress = false;
+            $scope.wasSuccess = data.Success;
+            $scope.wasError = !data.Success;
+            if ($scope.wasError) {
+                $scope.errorMessage = data.FailureReason || defaultError;
+            }
+        }
+
+        // Handles an error with the request.
+        function handleError() {
+            $scope.inProgress = false;
+            $scope.wasError = true;
+            $scope.wasSuccess = false;
+            $scope.errorMessage = defaultError;
+        }
 
     });
